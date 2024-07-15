109f6ff860409c519853a1057fa799e9c5a4b1ec
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-07-15 14:49:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	for (var i = 0; i < string.length; i++) {
+    var c = string.charAt(i);
+    if (string.indexOf(c) == i && string.indexOf(c, i + 1) == -1) {
+      return c;
+    }
+  }
+  return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
