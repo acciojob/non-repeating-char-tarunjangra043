20e10d165e69c948c409516da6d57e5685ec975e
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-07-22 12:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function firstNonRepeatedChar(str) {
-	let n = str.length;
+    let charCount = {};
     
-    for (let i = 0; i < n; ++i) {
-        let found = true;
-        for (let j = 0; j < n; ++j) {
-            if (i !== j && str[i] === str[j]) {
-                found = false;
-                break;
-            }
-        }
-        
-        if (found) {
-            return i;
+    // Count the occurrences of each character
+    for (let i = 0; i < str.length; i++) {
+        if (charCount[str[i]]) {
+            charCount[str[i]]++;
+        } else {
+            charCount[str[i]] = 1;
         }
     }
-    return -1;
-
+    
+    // Find the first non-repeated character
+    for (let i = 0; i < str.length; i++) {
+        if (charCount[str[i]] === 1) {
+            return str[i];
+        }
+    }
+    
+    return null;
 }
 
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
